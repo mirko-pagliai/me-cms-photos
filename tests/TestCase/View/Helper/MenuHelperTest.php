eeba10579d7981083671d058520c896ca53377a6
mirko-pagliai <mirko.pagliai@gmail.com>
2020-12-15 14:30:12 +0100

    fixed

--- tests/TestCase/View/Helper/MenuHelperTest.php
+++ tests/TestCase/View/Helper/MenuHelperTest.php
@@ -15,37 +15,13 @@ declare(strict_types=1);
 
 namespace MeCms\Photos\Test\TestCase\View\Helper;
 
-use MeTools\TestSuite\HelperTestCase;
-use MeTools\View\Helper\HtmlHelper;
+use MeCms\TestSuite\MenuHelperTestCase;
 
 /**
  * MenuHelperTest class
  */
-class MenuHelperTest extends HelperTestCase
+class MenuHelperTest extends MenuHelperTestCase
 {
-    /**
-     * Internal method to write auth data on session
-     * @param array $data Data you want to write
-     * @return void
-     */
-    protected function writeAuthOnSession(array $data = []): void
-    {
-        $this->Helper->getView()->getRequest()->getSession()->write('Auth.User', $data);
-        $this->Helper->Auth->initialize([]);
-    }
-
-    /**
-     * Internal method to build links
-     * @param array $links Links
-     * @return string
-     */
-    protected function buildLinks(array $links): string
-    {
-        return implode(PHP_EOL, array_map(function (array $link) {
-            return call_user_func_array([$this->getMockForHelper(HtmlHelper::class, null), 'link'], $link);
-        }, $links));
-    }
-
     /**
      * Tests for `photos()` method
      * @test
