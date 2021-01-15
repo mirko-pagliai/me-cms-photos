e112254a22d53f0cb9d5e6eee09822a3589afdd4
mirko-pagliai <mirko.pagliai@gmail.com>
2021-01-15 15:04:19 +0100

    fixed migrations

---
+++ config/Migrations/20210115140403_Initial.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up()
+    {
+        $this->table('photos')
+            ->addColumn('album_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('filename', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('description', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('active', 'boolean', [
+                'default' => true,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'filename',
+                ],
+                ['unique' => true]
+            )
+            ->addIndex(
+                [
+                    'album_id',
+                ]
+            )
+            ->create();
+
+        $this->table('photos_albums')
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => false,
+            ])
+            ->addColumn('slug', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => false,
+            ])
+            ->addColumn('description', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('photo_count', 'integer', [
+                'default' => '0',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down()
+    {
+        $this->table('photos')->drop()->save();
+        $this->table('photos_albums')->drop()->save();
+    }
+}
