d6ce5db6dedffdece4c379db989a676c2e8ea341
mirko-pagliai <mirko.pagliai@gmail.com>
2022-05-24 16:32:05 +0200

    test

--- tests/Fixture/PhotosFixture.php
+++ tests/Fixture/PhotosFixture.php
@@ -96,12 +96,10 @@ class PhotosFixture extends TestFixture
      */
     public function insert(ConnectionInterface $connection)
     {
+        $Filesystem = new Filesystem();
+
         foreach ($this->records as $record) {
-            $file = Filesystem::instance()->concatenate(PHOTOS, (string)$record['album_id'], $record['filename']);
-            if (!file_exists($file)) {
-                @mkdir(dirname($file), 0777, true);
-                @copy(WWW_ROOT . 'img' . DS . 'image.jpg', $file);
-            }
+            $Filesystem->copy(WWW_ROOT . 'img' . DS . 'image.jpg', $Filesystem->concatenate(PHOTOS, (string)$record['album_id'], $record['filename']));
         }
 
         return parent::insert($connection);
